e902f225d254db4ffe95d7f41aa6a6a4b7bb6a94
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2022-02-22 16:43:10 +0300

    Admin site customization

--- AppReviews/admin.py
+++ AppReviews/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+# admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Company)
+admin.site.register(ProductSite)
+admin.site.register(ProductSize)
+admin.site.register(Comment)
+
+admin.site.site_header = "Product Review Admin"
+
+
+@admin.register(Product)
+class ProductAmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'content',)
+    # list_filter = ('category',)
--- AppReviews/models.py
+++ AppReviews/models.py
@@ -1,9 +1,3 @@
-from itertools import product
-from nturl2path import url2pathname
-from tkinter import CASCADE
-from typing import OrderedDict
-from unicodedata import category, name
-from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth.models import User
 
